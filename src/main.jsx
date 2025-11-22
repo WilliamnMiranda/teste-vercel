44ef2112af49e99f681c7d3adf0b3f7d620247d3
William Nascimento <williamnnmiranda@gmail.com>
2025-11-21 23:50:04 -0300

    a

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-import { createHashRouter, RouterProvider } from "react-router";
+import { createBrowserRouter, createHashRouter, RouterProvider } from "react-router";
 
 import Main from "./routes/main/index.jsx";
 import Register from "./routes/register/index.jsx";
 import Data from "./routes/dados/index.jsx";
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
